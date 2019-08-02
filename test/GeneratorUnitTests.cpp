dfdecca251c90bfbaf0b01b4af072e32d1ba7075
dAmihl <eggerm@IBK-EDV-NB04.localdomain>
2019-08-02 16:06:47 +0200

    Added unit test for checkCreatesCircularDependency
     Committer: dAmihl <m.egger@e-mx.at>

--- test/GeneratorUnitTests.cpp
+++ test/GeneratorUnitTests.cpp
@@ -56,6 +56,20 @@ TEST_CASE("PuzzleGeneratorHelperUnitTests", "[PPG_UNIT_TEST]") {
 
 		}
 
+		WHEN("No circular dependency would be created, by adding N3->N4") {
+
+			R->addPair(N1, N2);
+			R->addPair(N2, N3);
+
+			T_PuzzleNodePair circularDependencyPair = PuzzleRelation::makePuzzlePair(N3, N4);
+
+			THEN("Return true in _checkCreatesCircularDependency()") {
+				bool hasCircularDependency = PuzzleGeneratorHelper::_checkCreatesCircularDependency(circularDependencyPair, R);
+				REQUIRE_FALSE(hasCircularDependency);
+			}
+
+		}
+
 		WHEN("Looking for a node in sequential order starting from N1-N2") {
 
 			T_PuzzleNodePair n1n2Pair = PuzzleRelation::makePuzzlePair(N1, N2);
@@ -66,6 +80,10 @@ TEST_CASE("PuzzleGeneratorHelperUnitTests", "[PPG_UNIT_TEST]") {
 
 			THEN("Return true because node N3 can be found") {
 				REQUIRE(PuzzleGeneratorHelper::_findNodeSequential(N3, n1n2Pair, R));
+			}
+
+			THEN("Return true because node N2 can be found in the starting node") {
+				REQUIRE(PuzzleGeneratorHelper::_findNodeSequential(N2, n1n2Pair, R));
 			}
 		}
 
