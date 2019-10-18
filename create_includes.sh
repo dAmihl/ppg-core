94d40d90fc9bcdb130d6a022e36bea07775956ad
dAmihl <eggerm@IBK-EDV-NB04.localdomain>
2019-10-18 11:42:42 +0200

    added includes folder for ppg-core headers

---
+++ create_includes.sh
@@ -0,0 +1,3 @@
+mkdir includes/ppg-core
+rm -rf includes/ppg-core/*
+cp src/*.h includes/ppg-core
---
+++ includes/ppg-core/PuzzleGenerator.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include "PuzzleNode.h"
+#include "PuzzleRelation.h"
+#include "PuzzleRandomizer.h"
+#include "PUZZGEN_TYPES.h"
+#include "Puzzle.h"
+#include "PuzzleGeneratorHelper.h"
+#include "PuzzleLogger.h"
+
+class PuzzleGenerator
+{
+public:
+	PuzzleGenerator();
+	~PuzzleGenerator();
+	
+	Puzzle* generatePuzzle(T_PuzzleObjectList objects, T_PuzzleEventList events, T_PuzzleRuleList rules);
+
+	PuzzleRelation* __simple_generateRelation(T_PuzzleNodeList nodes);
+
+	PuzzleRelation* generateRelation(T_PuzzleNodeList nodes, T_PuzzleRuleList rules);
+	PuzzleRelation* generateRelationExperimental(Puzzle* P, T_PuzzleNodeList nodes, T_PuzzleRuleList rules);
+
+	T_PuzzleNodeList generateNodes(T_PuzzleObjectList objects);
+
+	void setNumberNodes(int n);
+	int getNumberNodes();
+
+	void setSeed(unsigned int seed);
+	unsigned int getSeed();
+
+private:
+
+	void _initializeActivePropertyOnNodes(Puzzle* P);
+
+	void _cleanupNodes(Puzzle* P);
+	void _removeNodeFromList(PuzzleNode*N, T_PuzzleNodeList &nodes);
+	int NUM_NODES = 0;
+	unsigned int seed = 0;
+	bool seedSet = false;
+};
+
