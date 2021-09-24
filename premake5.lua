04a2bfde6d9645b0d203169dee6d324c2e93e330
dAmihl <m.egger@e-mx.at>
2021-09-24 20:53:08 +0200

    added small example impl

--- premake5.lua
+++ premake5.lua
@@ -36,3 +36,21 @@ project ("PPG-tests")
 	filter "configurations:Release"
 		defines {"NDEBUG"}
 		symbols "On"
+		
+		
+--------------- Example
+project ("PPG-example")
+	location "example"
+	targetdir "bin"
+	kind "ConsoleApp"
+	files {"example/*.cpp"}
+	includedirs {"src"}
+	links {"PPG"}
+	
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines {"NDEBUG"}
+		symbols "On"
